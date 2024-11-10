a7a98b0e426e80dc66c05808163ef9a457f65acd
Cahit Cihan SANDAL <59970676+sandalc@users.noreply.github.com>
2024-11-10 11:55:14 +0100

    Update Program.cs

--- QuartzASPNetCSharp/Program.cs
+++ QuartzASPNetCSharp/Program.cs
@@ -8,20 +8,7 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        /* Projede Quartz.NET kullanarak i?lerin s?ral? çal??mas?n? sa?lamak için 
-           bir ASP.NET Core uygulamas? olu?turduk. 
-           Projede üç farkl? i?(DbUpdateTask, ElastichIndexUpdateTask, IliskiTask) s?ras?yla çal??t?r?l?yor
-                  
-           JobListener ve chaining (zincirleme) kombinasyonu kullan?r. 
-           ??ler s?ral? ?ekilde çal??t?r?lacak, 
-           ayn? zamanda her i?in ba?lang?ç, tamamlanma ve hata durumlar? JobListener ile izlenip loglanacak (Console).
-
-           E?er Quartz.NET ile belirli bir aral?kla (örne?in her 3 saatte bir) bir i?in tetiklenmesini istiyorsan?z, 
-           bunun için bir zamanlay?c? (trigger) olu?turman?z gerekecek. 
-           Bu zamanlay?c?y?, TriggerBuilder kullanarak belirli bir aral?kta çal??acak ?ekilde yap?land?rabilirsiniz
-
-        */
-
+     
         // Add services to the container.
         IServiceCollection serviceCollection = builder.Services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
         IServiceCollection serviceCollection1 = builder.Services.AddHostedService<QuartzHostedService>();
